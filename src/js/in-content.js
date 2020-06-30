58cf8374699833e40c9e7a803fc6a367302e5b01
Hunter Hillman <hthillman@Hunters-MacBook-Pro.local>
2020-06-30 12:09:19 -0700

    outline of looper

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -43,14 +43,65 @@ const handleBackgroundResponse = response =>
 //     alert(start, end)
 // }
 
-const handleFormData = (e) => {
-        console.log('formdata event fired');
-       
-        // Get the form data from the event object
-        let data = e.formData;
-        for (var value of data.values()) {
-          alert(value);
+const handleSubmit = (e) => {
+    console.log('formdata event fired');
+
+    
+    // Get the form data from the event object
+    let data = e.formData;
+    for (var value of data.values()) {
+        alert(value);
+    }
+
+    this.looping = true
+    setYoutubeLoop(value[0], value[1])
+}
+
+const toggleFormDisplay = () =>{
+    let formSubmitButton = document.getElementsByName('submitTimestampInput')
+    let stopRunningLoopButton = document.getElementsByName('stopRunningLoop')
+    if (formSubmitButton.style.display === "none") {
+        formSubmitButton.style.display = "block";
+        stopRunningLoopButton.style.display = "none";
+      } else {
+        formSubmitButton.style.display = "none";
+        stopRunningLoopButton.style.display = "block";
+    }
+}
+
+const convertTimeInput = (time) => {
+    let number = time.replace(":","").parseFloat();
+    if(number.length < 1 || number.length > 6) throw "Please enter a valid timestamp"
+    if(number.length > 3) return number
+    if(number.length > 2 && number.length <5){
+        let minutes = String(number).slice(0, number.length - 2)
+        return (minutes*60 + String(number).slice(number.length - 2, number.length))
+    }
+    if(number.length > 4 && number.length <5){
+        let hours = String(number).slice(0, number.length - 4)
+        let minutes = String(number).slice(0, number.length - 2)
+        return ((hours*3600) + (minutes*60) + String(number).slice(number.length - 2, number.length))
+    }
+}
+
+
+const setYoutubeLoop = (start, end) =>{
+    while(this.looping){
+        let YT = document.getElementById("movie_player");
+        let currentTime = YT.getCurrentTime();
+        let startSeconds = convertTimeInput(start);
+        let endSeconds = convertTimeInput(end);
+    
+        while(currentTime < endSeconds){
+            currentTime = YT.getCurrentTime();
         }
+    
+        window.location.href = `&t=${startSeconds}`
+    }
+}
+
+const stopYoutubeLoop = () =>{
+    this.looping = false;
 }
 
 const loadPluginForm = () =>{
@@ -61,12 +112,26 @@ const loadPluginForm = () =>{
     var startTime = document.createElement('input')
     var endTime = document.createElement('input')
     var submitForm = document.createElement('input')
+    var stopRunningLoop = document.createElement('button')
+
+
+    // set button defaults
+    stopRunningLoop.name = "stopRunningLoop";
+    stopRunningLoop.innerText = "Stop Looper";
+    stopRunningLoop.style.display = "none"; // hidden at first
+
+    // add button click listener
+    stopRunningLoop.addEventListener ("click",stopYoutubeLoop);
 
     // Handle data from form
-    form.onformdata = handleFormData;
+    form.onformdata = handleSubmit;
+
+    //assign form name so we can hide it
+    form.name = "timestampInput"
 
     // assign default values for form input elements
     submitForm.type = 'submit';
+    submitForm.name = "submitTimestampInput"
     startTime.type = 'text';
     startTime.name = "startTime";
     startTime.placeholder = "0:00";
@@ -81,6 +146,7 @@ const loadPluginForm = () =>{
 
     //add form to wrapper div
     newNode.appendChild(form)
+    newNode.appendChild(stopRunningLoop)
 
     //insert new div with form at top of page
     let insertedNode = parentNode[0].insertBefore(newNode, firstChild);
